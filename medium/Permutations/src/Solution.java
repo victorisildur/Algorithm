87a94594513a5aa6760145e7700717a423b724dd
victorisildur <victorisildur@163.com>
2015-07-19 22:18:13 +0800

    complete permutation using dp

--- medium/Permutations/src/Solution.java
+++ medium/Permutations/src/Solution.java
@@ -1,9 +1,32 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Solution {
+	/*
+	 * {c} -> (c)
+	 * {b,c} -> insert b to every permutation of {c} 
+	 *       -> (b,c), (c,b)
+	 * {a,b,c} -> insert a to every permutaion of {b,c}
+	 *         -> (a,b,c) (b,a,c) (b,c,a)
+	 *         -> (a,c,b) (c,a,b) (c,b,a)
+	 */
 	public List<List<Integer>> permute(int []nums) {
-		List<List<Integer>> res = new ArrayList<ArrayList<Integer>>();
+		LinkedList<List<Integer>> res = new LinkedList<List<Integer>>();
+		List<Integer> perm0 = new ArrayList<Integer>();
+		perm0.add(nums[0]);
+		res.add(perm0);
+		for(int i=1; i<nums.length; i++) {
+			Integer curr = nums[i];
+			for(int j=0, prevPermsSize = res.size(); j<prevPermsSize; j++) {
+				List<Integer> prevPerm = res.pollFirst();
+				for(int k=0, permLength=prevPerm.size(); k<=permLength; k++) {
+					List<Integer> newPerm = new ArrayList<Integer>(prevPerm);
+					newPerm.add(k, curr);
+					res.add(newPerm);
+				}
+			}
+		}
 		return res;
 	}
 }
